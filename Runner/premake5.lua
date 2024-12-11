eb6300701f20c8ea1494c4aa0e02f5ff21af23a5
Luke Inlow <lukeinlow@gmail.com>
2024-12-11 11:36:33 -0500

    Fixed build file

--- Runner/premake5.lua
+++ Runner/premake5.lua
@@ -1,8 +1,8 @@
 project "Runner"
+    language "C++"
+    files { "**.cpp", "**.h" }
+    links { "opengl32", "SDL3", "liTechBase", "liTechFramework" }
     filter "configurations:Debug"
         kind "ConsoleApp"
     filter "configurations:Release"
         kind "WindowedApp"
-    language "C++"
-    files { "**.cpp", "**.h" }
-    links { "opengl32", "SDL3", "liTechBase", "liTechFramework" }
