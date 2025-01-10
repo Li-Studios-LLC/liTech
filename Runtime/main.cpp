c989beb3651d0b00698cabd53d518d3cd8f8f908
Luke Inlow <lukeinlow@gmail.com>
2025-01-10 14:44:31 -0500

    Switched to SDL main callbacks

--- Runtime/main.cpp
+++ Runtime/main.cpp
@@ -1,42 +1,44 @@
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
 #include <liTechFramework/Typedefs.h>
 #include <liTechFramework/Utility/Heap.h>
 #include <liTechFramework/Graphics/GraphicsContext.h>
 
-int main(int argc, char** argv) {
+struct runtime_t {
+    SDL_Window* window;
+    liGraphicsContext* context;
+    int width, height;
+} rt;
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
     liHeap* heap = liHeap::Instance();
-    {
-        SDL_Init(SDL_INIT_VIDEO);
-        std::string title = "liTech " + std::to_string(LITECH_MAJOR) + '.' + std::to_string(LITECH_MINOR) + '.' + std::to_string(LITECH_REVISION);
-        int width = 1280;
-        int height = 800;
-        SDL_Window* window = SDL_CreateWindow(title.c_str(), width, height, SDL_WINDOW_OPENGL);
-        bool running = true;
-        liGraphicsContext context = liGraphicsContext(window);
+    rt.width = 1280;
+    rt.height = 720;
+    std::string title = "liTech " + std::to_string(LITECH_MAJOR) + '.' + std::to_string(LITECH_MINOR) + '.' + std::to_string(LITECH_REVISION);
+    SDL_Init(SDL_INIT_VIDEO);
+    rt.window = SDL_CreateWindow(title.c_str(), rt.width, rt.height, SDL_WINDOW_OPENGL);
+    rt.context = liNew<liGraphicsContext>(rt.window);
 
-        while(running) {
-            SDL_Event ev;
-            while (SDL_PollEvent(&ev)) {
-                switch (ev.type) {
-                    case SDL_EVENT_QUIT:
-                        running = false;
-                        break;
-                    case SDL_EVENT_KEY_DOWN:
-                    case SDL_EVENT_KEY_UP:
-                    case SDL_EVENT_MOUSE_MOTION:
-                    case SDL_EVENT_MOUSE_BUTTON_DOWN:
-                    case SDL_EVENT_MOUSE_BUTTON_UP:
-                        break;
-                }
-            }
+    return SDL_APP_CONTINUE;
+}
 
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+SDL_AppResult SDL_AppIterate(void* appstate) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            context.Swap();
-        }
-        
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+    rt.context->Swap();
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
+    switch (event->type) {
+    case SDL_EVENT_QUIT:
+        return SDL_APP_SUCCESS;
+    default:
+        return SDL_APP_CONTINUE;
     }
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+    liDelete(rt.context);
     LITECH_DELETE_INSTANCE(liHeap);
-    return 0;
 }
