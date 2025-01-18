e6be097ccf9a7fe32a7728817c6d2714bfc87083
Luke Inlow <lukeinlow@gmail.com>
2025-01-17 21:57:27 -0500

    Added some more functions to the geometry class

--- liTechFramework/Math/Geometry.h
+++ liTechFramework/Math/Geometry.h
@@ -14,11 +14,13 @@ public:
     using VertexList = std::vector<VertexType>;
 
     liGeometry() {
+        this->hash = 0;
     }
 
     liGeometry(VertexList vertices, liUIntBuffer indices) {
         this->vertices = vertices;
         this->indices = indices;
+        CalculateHash();
     }
 
     ~liGeometry() {
@@ -26,21 +28,44 @@ public:
 
     void AddVertex(VertexType vertex) {
         vertices.push_back(vertex);
+        CalculateHash();
     }
 
     void AddIndex(uint_t index) {
         indices.push_back(index);
+        CalculateHash();
+    }
+
+    void AddPoint(VertexType vertex) {
+        vertices.push_back(vertex);
+        CalculateHash();
+    }
+
+    void AddLine(VertexType point0, VertexType point1) {
+        vertices.push_back(point0);
+        vertices.push_back(point1);
+        CalculateHash();
+    }
+
+    void AddTriangle(VertexType point0, VertexType point1, VertexType point2) {
+        vertices.push_back(point0);
+        vertices.push_back(point1);
+        vertices.push_back(point2);
+        CalculateHash();
     }
 
     void Clear() {
         vertices.clear();
         indices.clear();
+        this->hash = 0;
     }
 
     void CalculateHash() {
         this->hash = 0;
     }
 
+    LITECH_INLINE ulong_t VertexCount() const { return vertices.size(); }
+    LITECH_INLINE ulong_t IndexCount() const { return indices.size(); }
     LITECH_INLINE ulong_t Hash() const { return hash; }
 private:
     std::vector<VertexType> vertices;
