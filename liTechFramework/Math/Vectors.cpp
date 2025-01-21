3e61ef2ad0717f9fe42912b28bd89e815d1a3839
Luke Inlow <lukeinlow@gmail.com>
2025-01-21 00:05:30 -0500

    Added vector copy constructors

--- liTechFramework/Math/Vectors.cpp
+++ liTechFramework/Math/Vectors.cpp
@@ -5,6 +5,11 @@ liVector2f::liVector2f(float x, float y) {
     this->y = y;
 }
 
+liVector2f::liVector2f(const liVector2f& vec) {
+    this->x = vec.x;
+    this->y = vec.y;
+}
+
 float liVector2f::Length() const {
     return sqrtf(x * x + y * y);
 }
@@ -13,6 +18,12 @@ liVector3f::liVector3f(float x, float y, float z) {
     this->x = x;
     this->y = y;
     this->z = z;
+}
+
+liVector3f::liVector3f(const liVector3f& vec) {
+    this->x = vec.x;
+    this->y = vec.y;
+    this->z = vec.z;
 }
 
 float liVector3f::Length() const {
@@ -24,6 +35,13 @@ liVector4f::liVector4f(float x, float y, float z, float w) {
     this->y = y;
     this->z = z;
     this->w = w;
+}
+
+liVector4f::liVector4f(const liVector4f& vec) {
+    this->x = vec.x;
+    this->y = vec.y;
+    this->z = vec.z;
+    this->w = vec.w;
 }
 
 float liVector4f::Length() const {
--- liTechFramework/Math/Vectors.h
+++ liTechFramework/Math/Vectors.h
@@ -7,6 +7,7 @@ struct liVector2f {
     float x, y;
 
     liVector2f(float x = 0, float y = 0);
+    liVector2f(const liVector2f& vec);
 
     float Length() const;
 
@@ -17,6 +18,7 @@ struct liVector3f {
     float x, y, z;
 
     liVector3f(float x = 0, float y = 0, float z = 0);
+    liVector3f(const liVector3f& vec);
 
     float Length() const;
     
@@ -27,6 +29,7 @@ struct liVector4f {
     float x, y, z, w;
 
     liVector4f(float x = 0, float y = 0, float z = 0, float w = 0);
+    liVector4f(const liVector4f& vec);
 
     float Length() const;
 
